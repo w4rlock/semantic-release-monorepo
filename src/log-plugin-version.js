c53fa26b068752703f0e46a415098a46c36f88c5
Emanuel Rosal <emanuel.rosal@externos-cl.cencosud.com>
2023-03-08 15:28:32 -0300

    fix: move lib to dependency

--- src/log-plugin-version.js
+++ src/log-plugin-version.js
@@ -4,15 +4,11 @@ import readPkg from 'read-pkg';
 
 const logDebug = debug('semantic-release:monorepo');
 
-const logPluginVersion = (type) => (plugin) => async (pluginConfig, config) => {
+export const logPluginVersion = (type) => (plugin) => async (pluginConfig, config) => {
   if (config.options.debug) {
     const { version } = await readPkg(resolve(__dirname, '../'));
     logDebug('Running %o version %o', type, version);
   }
 
   return plugin(pluginConfig, config);
-};
-
-module.exports = {
-  logPluginVersion
 };
