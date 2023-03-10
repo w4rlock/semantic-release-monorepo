f54f265c283047869f70171267baa8d6f2abe0e7
Emanuel Rosal <emanuel.rosal@externos-cl.cencosud.com>
2023-03-09 23:53:58 -0300

    fix: remove __dirname

--- src/log-plugin-version.js
+++ src/log-plugin-version.js
@@ -1,12 +1,13 @@
 import debug from 'debug';
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
 import readPkg from 'read-pkg';
 
 const logDebug = debug('semantic-release:monorepo');
 
 export default (type) => (plugin) => async (pluginConfig, config) => {
   if (config.options.debug) {
-    const { version } = await readPkg(resolve(__dirname, '../'));
+    const dirname = dirname(module.filename);
+    const { version } = await readPkg(resolve(dirname, '../'));
     logDebug('Running %o version %o', type, version);
   }
 
